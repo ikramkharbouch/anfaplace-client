d6b1ce11e92ae3c37f8627ca90135785b3225634
Anis Bouzahar <bouzahar.anis@gmail.com>
2020-12-12 17:36:21 +0100

    indent fix and fixed uncommitted icons and style

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import './App.css';
 
 import BrandsGrid from './Components/BrandsGrid';
 import Brand from './Screens/Brand';
+import NavBar from './Components/NavBar';
+import BottomNav from './Components/BottomNav';
 
 const App = () => (
   <div className="App">
+    <NavBar />
     <Router>
       <Switch>
         <Route exact path="/brand" component={BrandsGrid} />
         <Route exact path="/brand/:id" component={Brand} />
       </Switch>
     </Router>
+    <BottomNav />
   </div>
 );
 export default App;
