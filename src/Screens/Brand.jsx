ff029b20000b566bc849fa8686f1f81a63baa69a
Nebur242 Nzaou <rm.nzaou.etudes@gmail.com>
2020-12-12 16:30:12 +0100

    BrandGrid component

---
+++ src/Screens/Brand.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Brand = () => <h2> Brand </h2>;
+
+export default Brand;
---
+++ src/login/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+
+const index = () => <Button primary> hello </Button>;
+
+export default index;
