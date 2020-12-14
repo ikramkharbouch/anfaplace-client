5045b30639bfdbf70108507d015e15c7a0b6648a
Anis Bouzahar <bouzahar.anis@gmail.com>
2020-12-14 10:32:37 +0100

    minior optimisation: - style du modale is overrided in semantic ui  config files - svg imported using  import { ReactComponent as NomIcon } from 'path/../../icon.svg' c'est mieux que hard coded. - faut metre les svg dans un button la taille du svg est trop petit sinon ca sera presque impossible de clicker sur mobile.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Button } from 'semantic-ui-react';
 
 import './App.css';
 
@@ -10,18 +11,30 @@ import Modal from './Components/Modal';
 import NavBar from './Components/NavBar';
 import BottomNav from './Components/BottomNav';
 
-const App = () => (
-  <div className="App">
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/brand" component={BrandsGrid} />
-        <Route exact path="/brand/:id" component={Brand} />
-        <Route exact path="/modal" component={Modal} />
-        <Route exact path="/" component={Home} />
-      </Switch>
-      <BottomNav />
-    </Router>
-  </div>
-);
+const App = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/brand" component={BrandsGrid} />
+          <Route exact path="/brand/:id" component={Brand} />
+          <Route exact path="/modal">
+            <Button onClick={() => setModalOpen(true)}>open Modal</Button>
+            <Modal
+              setOpen={(open) => {
+                console.log('open', open);
+                setModalOpen(open);
+              }}
+              open={modalOpen}
+            />
+          </Route>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        <BottomNav />
+      </Router>
+    </div>
+  );
+};
 export default App;
--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
 import { TransitionModal } from 'semantic-ui-react-transition-modal';
 import PropTypes from 'prop-types';
-
+import { ReactComponent as CloseIcon } from '../../assets/icons/close.svg';
 
 import './Modal.less';
 
-
-const index = ({ children, open, setOpen }) => (
-
-    <TransitionModal
-        animation="scale"
-        duration={350}
-        onClose={() => setOpen(false)}
-        onOpen={() => setOpen(true)}
-        open={open}
-        closeIcon={<div className='d-flex justify-content-end px-2 pt-10px'><CloseIcon setOpen={setOpen} /></div>}
-        className='custom-modal'
-    >
-        <TransitionModal.Content >
-            <p>hello</p>
-            {children}
-        </TransitionModal.Content>
-    </TransitionModal>
-)
-
-const CloseIcon = ({ setOpen }) => (
-    <svg onClick={() => setOpen(false)} version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"> <path fill="#fff" d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" />   </svg >
+const Modal = ({ children, open, setOpen }) => (
+  <TransitionModal
+    animation="scale"
+    duration={350}
+    onClose={() => setOpen(false)}
+    onOpen={() => setOpen(true)}
+    open={open}
+    closeIcon={<CloseButton setOpen={setOpen} />}
+    className="custom-modal"
+  >
+    <TransitionModal.Content>
+      <p>hello</p>
+      {children}
+    </TransitionModal.Content>
+  </TransitionModal>
 );
-
-CloseIcon.propTypes = {
-    setOpen: PropTypes.func.isRequired,
+const CloseButton = ({ setOpen }) => (
+  <div className="close">
+    <button
+      type="button"
+      onClick={() => {
+        setOpen(false);
+      }}
+    >
+      <CloseIcon />
+    </button>
+  </div>
+);
+CloseButton.propTypes = {
+  setOpen: PropTypes.func.isRequired,
+};
+Modal.propTypes = {
+  setOpen: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  open: PropTypes.bool,
+};
+Modal.defaultProps = {
+  open: false,
 };
 
-export default index;
+export default Modal;
