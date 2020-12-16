c8802224520f8117e192ca91b14a76f7e457f62d
Anis Bouzahar <bouzahar.anis@gmail.com>
2020-12-16 08:19:01 +0100

    qr code done.

--- src/Screens/QRcode/VerificationModal.jsx
+++ src/Screens/QRcode/VerificationModal.jsx
@@ -1,28 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Grid } from 'semantic-ui-react';
+import { Form, Input, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
 import Modal from '../../Components/Modal';
 import './VerificationModal.less';
 
-const VerificationModal = ({ open, setOpen }) => (
-  <Modal open={open} setOpen={setOpen}>
+const ConfirmationTel = ({ confirm }) => (
+  <>
     <p>Valider votre numéro de téléphone et commencer à collecter des points Anfapoints</p>
 
-    <Form className="verification-form">
-      <Grid>
-        <Grid.Column width={4} padded>
-          <Form.Input placeholder="+212" />
-        </Grid.Column>
-        <Grid.Column width={12} padded>
-          <Form.Input placeholder="Numero tel" type="tel" />
-        </Grid.Column>
-      </Grid>
+    <Form onSubmit={confirm} className="verification-form">
+      <Form.Group width={16} unstackable>
+        <Form.Field width={4} inline>
+          <Input placeholder="+212" />
+        </Form.Field>
+
+        <Form.Field width={12} inline>
+          <Input type="tel" />
+        </Form.Field>
+      </Form.Group>
+      <Link to="/" className="tos">
+        Politique de confidentialité
+      </Link>
       <Form.Checkbox label="Opt-in whatsapp +200 points" />
       <Form.Checkbox label="Validation par SMS +100 points" />
-      <Form.Button circular>Confirmer mon numéro</Form.Button>
+      <Form.Button className="confirmer" circular>
+        Confirmer mon numéro
+      </Form.Button>
     </Form>
-  </Modal>
+  </>
 );
+ConfirmationTel.propTypes = {
+  confirm: PropTypes.func.isRequired,
+};
+const PinVerification = () => (
+  <>
+    <p>Vérification de votre numéro de téléphone</p>
+    <Form className="pin-verification">
+      <Form.Group className="digits" unstackable widths={16}>
+        <Form.Field width={4}>
+          <Input maxlength={1} type="number" />
+        </Form.Field>
+        <Form.Field width={4}>
+          <Input maxlength={1} type="number" />
+        </Form.Field>
+        <Form.Field width={4}>
+          <Input maxlength={1} type="number" />
+        </Form.Field>
+        <Form.Field width={4}>
+          <Input maxlength={1} type="number" />
+        </Form.Field>
+      </Form.Group>
+
+      <Form.Field>
+        <Button circular className="continue">
+          Continuer
+        </Button>
+      </Form.Field>
+      <span className="retry">
+        Je n’ai pas reçu de code.<Link to="/"> Renvoyer le code</Link>
+      </span>
+    </Form>
+  </>
+);
+
+const VerificationModal = ({ open, setOpen }) => {
+  const [verifyPin, setVerifyPin] = useState(false);
+
+  return (
+    <Modal open={open} setOpen={setOpen}>
+      {!verifyPin && <ConfirmationTel confirm={() => setVerifyPin(true)} />}
+      {verifyPin && <PinVerification />}
+    </Modal>
+  );
+};
 VerificationModal.propTypes = {
   open: PropTypes.func.isRequired,
   setOpen: PropTypes.func.isRequired,
