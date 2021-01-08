42913da53e49355aadeecf352932f69677bf28cb
Nebur242 Nzaou <rm.nzaou.etudes@gmail.com>
2021-01-08 01:01:01 +0100

    promo checkox

--- src/components/BrandsGrid/index.jsx
+++ src/components/BrandsGrid/index.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Brand from '../Brand';
 import './BrandsGrid.less';
 
-const BrandsGrid = ({ brands }) => (
-	<div className="brands__grid">
-		{brands.map((brand) => (
-			<Brand
-				withBadge={!!brand.data.Tags[0]?.title}
-				badgeColor={brand.data.Tags[0]?.color}
-				badgeText={brand.data.Tags[0]?.title}
-				brandImg={brand.data.logo.data}
-				brandName={brand.data.titre}
-				brandId={brand.index}
-				imageType={brand.data.logo.type}
-			/>
-		))}
-	</div>
-);
+const BrandsGrid = ({ brands, promo }) => {
+
+	const [brandList, setBrandList] = useState([]);
+	console.log(brands)
+
+	useEffect(() => {
+		if (!promo) {
+			setBrandList(brands);
+		} else {
+			setBrandList(brands.filter(brd => brd?.data?.enPromotion))
+		}
+
+	}, [brands, promo]);
+
+	return (
+		<div className="brands__grid">
+			{brandList.map((brand) => (
+				<Brand
+					withBadge={!!brand.data.Tags[0]?.title}
+					badgeColor={brand.data.Tags[0]?.color}
+					badgeText={brand.data.Tags[0]?.title}
+					brandImg={brand.data.logo.data}
+					brandName={brand.data.titre}
+					brandId={brand.index}
+					imageType={brand.data.logo.type}
+				/>
+			))}
+		</div>
+	);
+}
 BrandsGrid.propTypes = {
 	brands: PropTypes.arrayOf(
 		PropTypes.shape({
@@ -29,9 +44,11 @@ BrandsGrid.propTypes = {
 			imageType: PropTypes.string,
 		})
 	),
+	promo: PropTypes.bool,
 };
 BrandsGrid.defaultProps = {
 	brands: [],
+	promo: false
 };
 
 export default BrandsGrid;
