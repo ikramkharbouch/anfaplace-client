bee9b0d1e4ba94c4e4f9b9e7112dac69b64ad755
Ruben Nzaou <ruben-n@dba.ma>
2021-01-14 12:53:03 +0100

    limit brands in marque slider

--- src/Screens/Home/Sections/Brands/index.jsx
+++ src/Screens/Home/Sections/Brands/index.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React , {  useState , useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Button, Header } from 'semantic-ui-react';
+import { shuffle } from 'src/utils/utilsFunctions';
 import Slider from 'src/Components/Slider';
 import Brand from 'src/Components/Brand';
 import './Brands.less';
 
+
+
 const Brands = () => {
 	const brands = useSelector((state) => state.brand.all);
 	const history = useHistory();
+	const [ allBrands , setAllBrands ] = useState([]);
+
+	useEffect(() => {
+		setAllBrands(shuffle(brands))
+	} , [brands]);
+
 	return (
 		<div className="brands">
 			<Header as="h3">Marques</Header>
 			<Slider id="brands" autoplay={false} slidesPerGroup={2} slidesPerColumn={2} slidersPerView={2}>
-				{brands.map((marque) => (
+				{allBrands.filter( ( _ , index) => index < 20 ).map((marque) => (
 					<div key={marque.index} id={marque.data.nom} className="brand-slider-container">
 						<Brand
 							brandImg={marque?.data?.logo?.data || marque?.data?.logo}
