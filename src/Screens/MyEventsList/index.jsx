3756bb595516ac17fb3c0ef29477b2949d9a4f99
Ruben Nzaou <ruben-n@dba.ma>
2021-01-14 02:00:35 +0100

    add activate btn to ma liste event && ma liste visite when not connected

--- src/Screens/MyEventsList/index.jsx
+++ src/Screens/MyEventsList/index.jsx
@@ -3,7 +3,9 @@ import React, { useEffect , useState } from 'react';
 import EventCard from 'src/Components/EventCard';
 import './MyEventsList.less';
 import { useDispatch, useSelector } from 'react-redux';
-import { Dimmer, Loader } from 'semantic-ui-react';
+import { Dimmer, Loader , Button } from 'semantic-ui-react';
+import { openNumberVerificationModal, openPhoneAuth } from 'src/store/app';
+
 
 const MyEventsList = () => {
 	const dispatch = useDispatch();
@@ -16,13 +18,14 @@ const MyEventsList = () => {
 
 	useEffect(() => {
 		if (user && !eventList.success) {
-			dispatch( { type : 'GET_USER_EVENTS'  } );
+			dispatch( { type : 'GET_USER_EVENTS' });
 		}
 
 		if(eventList.success && !eventList.loading){
 			console.log(eventList.events)
 			setLoading(false)
 		}
+		setLoading(false)
 	}, [user , eventList.events]);
 
 	return loading ? (
@@ -34,8 +37,25 @@ const MyEventsList = () => {
 			{eventList.events.length ? (
 				eventList.events.map((event) => <EventCard key = { event.id } event={event} hasParticipated />)
 			) : (
-				<p style={{ textAlign: 'center' }}> aucun evenement </p>
+				user?.currentUser &&  <p style={{ textAlign: 'center' }}> aucun evenement </p>
 			)}
+
+			{ !user?.currentUser && <p style={{ textAlign: 'center' }}> Merci de vous connecter ! </p>}
+
+			{ !user?.currentUser && <div className="action" style = {{ display: 'flex' , justifyContent: 'center' }} >
+				<Button
+					circular
+					onClick={() => {
+						if (user) {
+							dispatch(openNumberVerificationModal(true));
+						} else {
+							dispatch(openPhoneAuth({ open: true, withEmail: true }));
+						}
+					}}
+				>
+					Activer mon compte
+				</Button>
+			</div> }
 		</div>
 	);
 };
